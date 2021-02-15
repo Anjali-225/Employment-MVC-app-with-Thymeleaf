5ecf8a63f0479ea68ba59403e74c01f4248d320b
Anjali <anjali.payal69@gmail.com>
2021-02-15 11:32:12 +0200

    Employment_Management_System

---
+++ src/main/java/com/anjali/springboot/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.anjali.springboot.service;
+
+import java.util.List;
+
+import com.anjali.springboot.model.Employee;
+import org.springframework.data.domain.Page;
+
+public interface EmployeeService {
+//	List<Employee> getAllEmployees();
+//
+//	void saveEmployee(Employee employee);
+//	Employee getEmployeeByID(long id);
+//	void deleteEmployeeByID(long id);
+
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+
+}
